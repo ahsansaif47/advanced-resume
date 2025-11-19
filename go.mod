4960b29fead2622de62ccc1f26e2be8d19fad397
ahsansaif47 <ahsan_saif47@hotmail.com>
2025-11-19 18:26:41 +0500

    parser done

--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module github.com/ahsansaif47/advanced-resume
 go 1.25.4
 
 require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/ebitengine/purego v0.8.4 // indirect
+	github.com/gen2brain/go-fitz v1.24.15 // indirect
+	github.com/jupiterrider/ffi v0.5.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+)
--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,1 +1,57 @@
 package parser
+
+import (
+	"bytes"
+	"image/png"
+
+	"github.com/gen2brain/go-fitz"
+)
+
+type Parser interface {
+	ExtractImages() ([]ImageData, error)
+	Close() error
+}
+
+type ImageData struct {
+	page int
+	Data []byte
+}
+
+type FitzParser struct {
+	Doc *fitz.Document
+}
+
+func NewFitzParser(path string) (*FitzParser, error) {
+	doc, err := fitz.New(path)
+	if err != nil {
+		return nil, err
+	}
+	return &FitzParser{Doc: doc}, nil
+}
+
+func (p *FitzParser) ExtractImages() ([]ImageData, error) {
+	totalPages := p.Doc.NumPage()
+
+	outputImages := make([]ImageData, totalPages)
+	for i := 0; i < totalPages; i++ {
+		img, err := p.Doc.Image(i)
+		if err != nil {
+			return nil, err
+		}
+
+		buf := new(bytes.Buffer)
+		png.Encode(buf, img)
+
+		outputImages = append(outputImages, ImageData{
+			page: i,
+			Data: buf.Bytes(),
+		})
+
+	}
+
+	return outputImages, nil
+}
+
+func (p *FitzParser) Close() error {
+	return p.Doc.Close()
+}
