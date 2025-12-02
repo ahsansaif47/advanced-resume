222a9822e0460e70ef81fe552c97e3cfc8b3e56a
ahsansaif47 <ahsan_saif47@hotmail.com>
2025-12-02 11:58:03 +0500

    repo layer done

--- internal/storage/weaviate/repository.go
+++ internal/storage/weaviate/repository.go
@@ -4,32 +4,56 @@ import (
 	"context"
 
 	"github.com/weaviate/weaviate-go-client/v5/weaviate"
+	"github.com/weaviate/weaviate/entities/models"
 )
 
 type IWeaviateRepository interface {
-	InsertData(ctx context.Context, className string, props map[string]any)
-	BatchInsert()
-	VectorSearch()
+	InsertData(className string, props map[string]any) (string, error)
+	BatchInsert(className string, items []map[string]any) error
+	VectorSearch(className, query string) (any, error)
 }
 
 type WeaviateRepository struct {
+	ctx    context.Context
 	Client *weaviate.Client
 }
 
-func NewWeviateRepository(client *weaviate.Client) IWeaviateRepository {
+func NewWeviateRepository(ctx context.Context, client *weaviate.Client) IWeaviateRepository {
 	return &WeaviateRepository{
+		ctx:    ctx,
 		Client: client,
 	}
 }
 
-func (r *WeaviateRepository) InsertData(ctx context.Context, className string, props map[string]any) {
+func (r *WeaviateRepository) InsertData(className string, props map[string]any) (string, error) {
+	model, err := r.Client.Data().Creator().WithClassName(className).WithProperties(props).Do(r.ctx)
+	if err != nil {
+		return "", err
+	}
 
+	return model.Object.ID.String(), nil
 }
 
-func (r *WeaviateRepository) BatchInsert() {
+func (r *WeaviateRepository) BatchInsert(className string, items []map[string]any) error {
 
+	batch := r.Client.Batch().ObjectsBatcher()
+	for _, item := range items {
+		batch = batch.WithObjects(&models.Object{
+			Class:      className,
+			Properties: item,
+		})
+	}
+
+	_, err := batch.Do(r.ctx)
+	return err
 }
 
-func (r *WeaviateRepository) VectorSearch() {
+func (r *WeaviateRepository) VectorSearch(className, query string) (any, error) {
+	response, err := r.Client.GraphQL().Get().
+		WithClassName("").
+		WithNearText(r.Client.GraphQL().NearTextArgBuilder().WithConcepts([]string{query})).
+		WithLimit(2).
+		Do(context.Background())
 
+	return response, err
 }
